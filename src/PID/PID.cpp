efaa1ff0c1b6925b30ab5182bb373c0137c9eb23
Flians <rongliangfu@outlook.com>
2021-03-10 09:43:41 +0800

    update PID

--- src/PID/PID.cpp
+++ src/PID/PID.cpp
@@ -1,113 +1,200 @@
-#include <limits>
-#include <iostream>
-#include "PID.h"
+#include "Pid.h"
 
-//using namespace std;
-
-PID::PID() {}
-
-PID::~PID() {}
-
-void PID::_init(double Kp, double Ki, double Kd)
+unsigned long millis()
 {
-    std::vector<double>().swap(this->p);
-    std::vector<double>().swap(this->dp);
-    std::vector<double>().swap(this->ep);
-    this->p = {Kp, Ki, Kd};
-    this->dp = {0.1 * Kp, 0.1 * Ki, 0.1 * Kd};
-    for (int i = 0; i < 3; ++i)
-    {
-        if (this->dp[i] == 0)
-            this->dp[i] = 1;
-    }
-    this->reset();
+    struct timeb t1;
+    ftime(&t1);
+    return t1.millitm + t1.time * 1000;
 }
 
-void PID::reset()
+PID::PID(double Kp, double Ki, double Kd)
 {
-    this->ep = {0, 0, 0};
+    this->inAuto = true;
+    this->kp.resize(3, 0);
+    this->ep.resize(3, 0);
+    PID::SetOutputLimits(-0.3, 0.3); //default output limit
 
-    this->step = 1;
-    this->index = 0;
-    this->fail = 0;
+    SampleTime = 50; //default Controller Sample Time is 0.05 seconds
 
-    this->best_error = std::numeric_limits<double>::max();
-    this->total_error = 0;
+    PID::SetTunings(Kp, Ki, Kd);
+
+    lastTime = millis() - SampleTime;
 }
 
-void PID::_update_error(double cte)
+double PID::Compute(double dth, double v, cv::Point cur, cv::Point A, cv::Point B)
 {
-    int T = 50;
-    int TI = 5;
-    if (this->step == 1)
+    if (!inAuto)
+        return false;
+    unsigned long now = millis();
+    unsigned long timeChange = (now - lastTime);
+    if (timeChange >= SampleTime)
     {
-        this->ep[0] = cte;
-    }
-    this->ep[2] = cte - this->ep[0];
-    this->ep[0] = cte;
-    this->ep[1] += cte;
-
-    if (step % T > TI)
-    {
-        this->total_error += cte * cte;
-    }
-    if (step % T == 0)
-    {
-        if (step == T)
+        double cte = distance_from_point_to_line(cur.x, cur.y, A.x, A.y, B.x, B.y);
+        cv::Point foot = get_foot_point(cur, A, B);
+        bool left = (foot.x - cur.x) * (B.y - A.y) - (B.x - B.y) * (foot.y - cur.y) > 0;
+        if (dth > M_PI)
         {
-            if (this->total_error < this->best_error)
-            {
-                this->best_error = this->total_error;
-            }
-            this->p[this->index] += this->dp[this->index];
+            dth -= 2 * M_PI;
+        }
+        else if (dth < -M_PI)
+        {
+            dth += 2 * M_PI;
+        }
+        /*Compute all the working error variables*/
+        this->ep[2] = this->kp[2] * dth;
+        this->ep[0] = this->kp[0] * cte;
+        this->ep[1] += this->kp[1] * cte * v * SampleTime / 1000.0;
+        /* limit integral*/
+        if (this->ep[1] > outMax)
+        {
+            this->ep[1] = outMax;
+        }
+        else if (this->ep[1] < outMin)
+        {
+            this->ep[1] = outMin;
+        }
+
+        /*Compute PID Output*/
+        double output;
+        if (left)
+        {
+            output = -this->ep[0] - this->ep[1] + this->ep[2];
         }
         else
         {
-            if (this->total_error < this->best_error)
-            {
-                this->best_error = this->total_error;
-                this->dp[this->index++] *= 1.1;
-                this->index %= 3;
-                this->p[this->index] += this->dp[this->index];
-                this->fail = 0;
-            }
-            else if (this->fail == 0)
-            {
-                this->p[this->index] -= (2 * this->dp[this->index]);
-                this->fail++;
-            }
-            else
-            {
-                this->p[this->index] += (2 * this->dp[this->index]);
-                this->dp[this->index++] *= 0.9;
-                this->index %= 3;
-                this->p[this->index] += this->dp[this->index];
-                this->fail = 0;
-            }
+            output = this->ep[0] + this->ep[1] + this->ep[2];
         }
-        std::cerr << "\033[31mcurr desision: "
-                  << "==============  step " << step << " ==============" << std::endl;
-        std::cerr << "\033[31mcurr desision: "
-                  << "P: " << this->p[0] << " I: " << this->p[1] << " D: " << this->p[2] << std::endl;
-        std::cerr << "\033[31mcurr desision: "
-                  << "best_error: " << best_error << " total_error: " << total_error << std::endl;
-        std::cerr << "\033[31mcurr desision: " << std::endl;
-        total_error = 0;
+
+        RoundTheta(output);
+        /* limit output*/
+        if (output > outMax)
+        {
+            output = outMax;
+        }
+        else if (output < outMin)
+        {
+            output = outMin;
+        }
+
+        /*Remember some variables for next time*/
+        lastTime = now;
+        return output;
     }
-    ++this->step;
+    else
+        return __DBL_MAX__;
 }
 
-double PID::_total_error()
+void PID::SetTunings(double Kp, double Ki, double Kd)
 {
-    double res = -this->p[0] * this->ep[0] - this->p[1] * this->ep[1] - this->p[2] * this->ep[2];
-    res = fmod(res, 2 * M_PI);
-    if (res > M_PI)
+    if (Kp < 0 || Ki < 0 || Kd < 0)
+        return;
+
+    double SampleTimeInSec = ((double)SampleTime) / 1000;
+    this->kp = {Kp, Ki * SampleTimeInSec, Kd / SampleTimeInSec};
+}
+
+/* SetSampleTime(...) *********************************************************
+ * sets the period, in Milliseconds, at which the calculation is performed
+ ******************************************************************************/
+void PID::SetSampleTime(int NewSampleTime)
+{
+    if (NewSampleTime > 0)
     {
-        res -= 2 * M_PI;
+        double ratio = (double)NewSampleTime / (double)SampleTime;
+        this->kp[1] *= ratio;
+        this->kp[2] /= ratio;
+        SampleTime = (unsigned long)NewSampleTime;
     }
-    else if (res <= -M_PI)
+}
+
+void PID::SetOutputLimits(double Min, double Max)
+{
+    if (Min >= Max)
+        return;
+    outMin = Min;
+    outMax = Max;
+
+    if (inAuto)
     {
-        res += 2 * M_PI;
+        if (this->ep[1] > outMax)
+            this->ep[1] = outMax;
+        else if (this->ep[1] < outMin)
+            this->ep[1] = outMin;
     }
-    return res;
+}
+
+/* SetMode(...)****************************************************************
+ * Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
+ * when the transition from manual to auto occurs, the controller is
+ * automatically initialized
+ ******************************************************************************/
+void PID::SetMode(int Mode)
+{
+    bool newAuto = (Mode == AUTOMATIC);
+    if (newAuto && !inAuto)
+    { /*we just went from manual to auto*/
+        PID::Initialize();
+    }
+    inAuto = newAuto;
+}
+
+/* Initialize()****************************************************************
+ *	does all the things that need to happen to ensure a bumpless transfer
+ *  from manual to automatic mode.
+ ******************************************************************************/
+void PID::Initialize()
+{
+    if (this->ep[1] > outMax)
+        this->ep[1] = outMax;
+    else if (this->ep[1] < outMin)
+        this->ep[1] = outMin;
+}
+
+double PID::RoundTheta(double angle)
+{
+    double a = fmod(angle + M_PI, 2.0 * M_PI);
+    if (a < 0.0)
+    {
+        a += (2.0 * M_PI);
+    }
+    return a - M_PI;
+}
+
+double PID::distance_from_point_to_line(
+    const double &x0, const double &y0,
+    const double &x1, const double &y1,
+    const double &x2, const double &y2)
+{
+    double d = (fabs((y2 - y1) * x0 + (x1 - x2) * y0 + ((x2 * y1) - (x1 * y2)))) /
+               (sqrt(pow(y2 - y1, 2) + pow(x1 - x2, 2)));
+    return d;
+}
+
+bool PID::point_in_left_line(const double &x0, const double &y0,
+                             const double &x1, const double &y1,
+                             const double &x2, const double &y2)
+{
+    return (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0) >= 0;
+}
+
+cv::Point2d PID::get_foot_point(cv::Point point, cv::Point pnt1, cv::Point pnt2)
+{
+    double A = pnt2.y - pnt1.y;                   //y2-y1
+    double B = pnt1.x - pnt2.x;                   //x1-x2;
+    double C = pnt2.x * pnt1.y - pnt1.x * pnt2.y; //x2*y1-x1*y2
+    if (A * A + B * B < 1e-13)
+    {
+        return cv::Point2d(pnt1.x, pnt1.y); //pnt1与pnt2重叠
+    }
+    else if (abs(A * point.x + B * point.y + C) < 1e-13)
+    {
+        return cv::Point2d(point.x, point.y); //point在直线上(pnt1_pnt2)
+    }
+    else
+    {
+        double x = (B * B * point.x - A * B * point.y - A * C) / (A * A + B * B);
+        double y = (-A * B * point.x + A * A * point.y - B * C) / (A * A + B * B);
+        cv::Point2d fpoint(x, y);
+        return fpoint;
+    }
 }
--- src/PID/PID.h
+++ src/PID/PID.h
@@ -1,19 +1,69 @@
-#ifndef _PID_H
-#define _PID_H
+#ifndef PID_H
+#define PID_H
 
 #include <cmath>
 #include <vector>
+#include <climits>
+#include <iostream>
+#include <sys/timeb.h>
+#include <opencv2/opencv.hpp>
+
+//Constants used in some of the functions below
+#define AUTOMATIC 1
+#define MANUAL 0
 
 class PID
 {
+
+public:
+    PID(double Kp, double Ki, double Kd);
+
+    void SetMode(int Mode); // * sets PID to either Manual (0) or Auto (non-0)
+
+    double Compute(double dth, double v, cv::Point cur, cv::Point A, cv::Point B); // * performs the PID calculation.  it should be
+                                                                                   //   called every time loop() cycles. ON/OFF and
+                                                                                   //   calculation frequency can be set using SetMode
+                                                                                   //   SetSampleTime respectively
+
+    void SetOutputLimits(double, double); // * clamps the output to a specific range. 0-255 by default, but
+                                          //   it's likely the user will want to change this depending on
+                                          //   the application
+
+    //available but not commonly used functions ********************************************************
+    void SetTunings(double, double, // * While most users will set the tunings once in the
+                    double);        //   constructor, this function gives the user the option
+                                    //   of changing tunings during runtime for Adaptive control
+
+    void SetControllerDirection(int); // * Sets the Direction, or "Action" of the controller. DIRECT
+                                      //   means the output will increase when error is positive. REVERSE
+                                      //   means the opposite.  it's very unlikely that this will be needed
+                                      //   once it is set in the constructor.
+    void SetSampleTime(int);          // * sets the frequency, in Milliseconds, with which
+                                      //   the PID calculation is performed.  default is 100
+
+    // [-pi, pi]
+    double RoundTheta(double angle);
+    // point (x0,y0) -> line (x1,y1), (x2,y2)
+    double distance_from_point_to_line(
+        const double &x0, const double &y0,
+        const double &x1, const double &y1,
+        const double &x2, const double &y2);
+    // point (x0,y0) -> line (x1,y1), (x2,y2)
+    bool point_in_left_line(const double &x0, const double &y0,
+                            const double &x1, const double &y1,
+                            const double &x2, const double &y2);
+    cv::Point2d get_foot_point(cv::Point point, cv::Point pnt1, cv::Point pnt2);
+
 private:
-    int step;
-    int index; // [0 -> p, 1 -> i, 2 -> d]
-    int fail;
+    void Initialize();
+
     /**
     * Coefficients, the order is P, I, D
+    * (P)roportional Tuning Parameter
+    * (I)ntegral Tuning Parameter
+    * (D)erivative Tuning Parameter
     */
-    std::vector<double> p;
+    std::vector<double> kp;
     /**
     * Coefficients, the order is dP, dI, dD
     */
@@ -22,36 +72,11 @@ private:
     * Coefficients, the order is eP, eI, eD
     */
     std::vector<double> ep;
-    double best_error;
-    double total_error;
 
-public:
-    /*
-    * Constructor
-    */
-    PID();
+    int controllerDirection;
 
-    /*
-    * Destructor.
-    */
-    virtual ~PID();
-
-    /*
-    * Initialize PID.
-    */
-    void _init(double Kp, double Ki, double Kd);
-
-    void reset();
-
-    /*
-    * Update the PID error variables given cross track error.
-    */
-    void _update_error(double cte);
-
-    /*
-    * Calculate the total PID error.
-    */
-    double _total_error();
+    unsigned long lastTime, SampleTime;
+    double outMin, outMax;
+    bool inAuto;
 };
-
 #endif /* PID_H */
